f4636f2f9dbea667943eef0ae443fbe591b4705f
yogaprasetyo27 <132985254+yogaprasetyo27@users.noreply.github.com>
2024-09-07 11:01:57 +0700

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,38 @@
+# a = "apel" # "a" adalah variabel dan "apel" adalah data
+# print(a)
+
+# buah = "jeruk" # ini adalah tipe data string
+# harga = 15000 # ini adalah tipe data integer
+# berat = 1.5 # ini adalah tipe data float
+# beli = True # ini adalah tipe data boolean
+
+# print(buah)
+# print(harga)
+# print(berat)
+# print(beli)
+
+# format string
+# nama = 'Yoga'
+# umur = 18
+# alamat = 'Jl.Buntu'
+# tinggi = 1.75
+
+# print(f"Nama saya {nama}. Saya berusia {umur} tahun, dan tinggi saya {tinggi} meter. Saya tinggal di {alamat}.")
+
+# input
+
+# nama = input("Siapa nama kamu?")
+# print(f"Hallo, {nama}")
+
+# umur = int(input("Umur kamu berapa?"))
+# print(f"{umur} tahun")
+
+print("Hallo, perkenalkan nama aku Bot!")
+print("Hallo, Bot!")
+nama = input("Nama, kamu siapa?")
+print(f"Namaku {nama}")
+print(f"Hallo, {nama}, salam kenal!")
+print(f"Kamu sibuk apa sekarang, {nama}?")
+kegiatan = input("Kesibukanmu?")
+print(f"Aku lagi sibuk {kegiatan} sekarang.")
+print(f"Oke, semoga {kegiatan} berhasil.")
